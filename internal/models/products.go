9f15a2354b8f311916be7b6a600e1c0f3a16b504
Sol Battaglia <solbattaglia@gmail.com>
2022-05-28 16:58:09 -0300

    Added struct tags

--- internal/models/products.go
+++ internal/models/products.go
@@ -1,9 +1,10 @@
 package models
 
 type Product struct {
-	ID    int
-	Name  string
-	Type  string
-	Count int
-	Price float64
+	ID    int     `json:"id,omitempty"`
+	UID   string  `json:"uid,omitempty"`
+	Name  string  `json:"name,omitempty"`
+	Type  string  `json:"type,omitempty"`
+	Count int     `json:"count,omitempty"`
+	Price float64 `json:"price,omitempty"`
 }
