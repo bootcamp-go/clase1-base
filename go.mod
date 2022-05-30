c08b1d567fad5346028675dbc0d8da70d6181a97
Sol Battaglia <solbattaglia@gmail.com>
2022-05-30 09:22:06 -0300

    Added go-txdb package implementation

--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module github.com/bootcamp-go/clase1-base
 go 1.13
 
 require (
+	github.com/DATA-DOG/go-txdb v0.1.5
 	github.com/gin-gonic/gin v1.7.7
 	github.com/go-sql-driver/mysql v1.6.0
-	github.com/joho/godotenv v1.4.0
+	github.com/google/uuid v1.3.0
+	github.com/lib/pq v1.10.6 // indirect
 )
---
+++ pkg/utils/txdb.go
@@ -0,0 +1,23 @@
+package utils
+
+import (
+	"database/sql"
+
+	"github.com/DATA-DOG/go-txdb"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/google/uuid"
+)
+
+func init() {
+	txdb.Register("txdb", "mysql", "root:password@/storage")
+}
+
+func InitDb() (*sql.DB, error) {
+	db, err := sql.Open("txdb", uuid.New().String())
+
+	if err == nil {
+		return db, db.Ping()
+	}
+
+	return db, err
+}
